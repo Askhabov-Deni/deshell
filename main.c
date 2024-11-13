df49d86276583f446e9444ec2610b8c1b128f511
Askhabov Deni <askhabovdenidev@gmail.com>
2024-11-14 00:41:33 +0300

    Версия shell c поддержкой <, >, >>, |

--- main.c
+++ main.c
@@ -9,7 +9,7 @@
 #define MAX_COMMAND_LENGTH 1024
 #define MAX_ARGS 64
 
-// Функция для перенаправления вывода в файл (`>`)
+// >
 void redirect_output(char *filename) {
     int fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0644);
     if (fd < 0) {
@@ -20,7 +20,7 @@ void redirect_output(char *filename) {
     close(fd);
 }
 
-// Функция для дописывания вывода в файл (`>>`)
+// >>
 void append_output(char *filename) {
     int fd = open(filename, O_WRONLY | O_CREAT | O_APPEND, 0644);
     if (fd < 0) {
@@ -31,7 +31,7 @@ void append_output(char *filename) {
     close(fd);
 }
 
-// Функция для перенаправления ввода из файла (`<`)
+// <
 void redirect_input(char *filename) {
     int fd = open(filename, O_RDONLY);
     if (fd < 0) {
@@ -42,7 +42,71 @@ void redirect_input(char *filename) {
     close(fd);
 }
 
+// |
+void conveyor(char *command) {
+    char *pipeline_commands[MAX_ARGS];
+    int num_pipeline_commands = 0;
+
+    // Разделяем команду по `|`
+    char *token = strtok(command, "|");
+    while (token != NULL) {
+        pipeline_commands[num_pipeline_commands++] = token;
+        token = strtok(NULL, "|");
+    }
+
+    int pipefd[2];
+    int fd_in = 0; 
+
+    for (int i = 0; i < num_pipeline_commands; i++) {
+        char *args[MAX_ARGS];
+        int argc = 0;
+        char *arg_token = strtok(pipeline_commands[i], " ");
+        while (arg_token != NULL && argc < MAX_ARGS - 1) {
+            args[argc++] = arg_token;
+            arg_token = strtok(NULL, " ");
+        }
+        args[argc] = NULL;
+
+        // Создаём пайп для следующей команды, если это не последняя команда
+        if (i < num_pipeline_commands - 1) {
+            if (pipe(pipefd) == -1) {
+                perror("Ошибка создания пайпа");
+                exit(EXIT_FAILURE);
+            }
+        }
+
+        pid_t pid = fork();
+
+        if (pid == 0) { // Дочерний процесс
+            if (fd_in != 0) { // Если это не первая команда
+                dup2(fd_in, STDIN_FILENO);
+                close(fd_in);
+            }
+            if (i < num_pipeline_commands - 1) { // Если это не последняя команда
+                dup2(pipefd[1], STDOUT_FILENO);
+                close(pipefd[1]);
+            }
+            close(pipefd[0]);
+
+            execvp(args[0], args);
+            perror("Ошибка выполнения команды");
+            exit(EXIT_FAILURE);
+        } else { // Родительский процесс
+            waitpid(pid, NULL, 0);
+            close(pipefd[1]);
+            fd_in = pipefd[0];
+        }
+    }
+}
+
+// Функция для выполнения команды
 void execute_command(char *command) {
+    
+    if (strchr(command, '|')) {
+        conveyor(command);
+        return;
+    }
+
     char *args[MAX_ARGS];
     int argc = 0;
     char *token = strtok(command, " ");
@@ -54,38 +118,35 @@ void execute_command(char *command) {
     }
     args[argc] = NULL;
 
+    // Проверка на наличие перенаправлений
+    for (int i = 0; i < argc; i++) {
+        if (strcmp(args[i], ">") == 0) { 
+            args[i] = NULL;
+            redirect_output(args[i + 1]);
+            break;
+        } else if (strcmp(args[i], ">>") == 0) {
+            args[i] = NULL;
+            append_output(args[i + 1]);
+            break;
+        } else if (strcmp(args[i], "<") == 0) { 
+            args[i] = NULL;
+            redirect_input(args[i + 1]);
+            break;
+        }
+    }
+
     pid_t pid = fork();
     if (pid == 0) { // Дочерний процесс
-        // Проверка на наличие перенаправлений
-        for (int i = 0; i < argc; i++) {
-            if (strcmp(args[i], ">") == 0) { // Перенаправление вывода
-                args[i] = NULL; // Обрезаем аргументы на месте символа '>'
-                redirect_output(args[i + 1]);
-                break;
-            } else if (strcmp(args[i], ">>") == 0) { // Дописывание в файл
-                args[i] = NULL;
-                append_output(args[i + 1]);
-                break;
-            } else if (strcmp(args[i], "<") == 0) { // Перенаправление ввода
-                args[i] = NULL;
-                redirect_input(args[i + 1]);
-                break;
-            }
-        }
-
-        // Выполняем команду
         execvp(args[0], args);
         perror("Ошибка выполнения команды");
         exit(EXIT_FAILURE);
-
     } else { // Родительский процесс
         int status;
-        waitpid(pid, &status, 0); // Ждём завершения дочернего процесса
+        waitpid(pid, &status, 0);
     }
 }
 
-
-// Функция для разбиения строки на команды по разделителю ';'
+// Функция для разбиения строки на команды по разделителю `;`
 void split_commands(char *input, char *commands[], int *num_commands) {
     char *token = strtok(input, ";");
     *num_commands = 0;
@@ -96,7 +157,7 @@ void split_commands(char *input, char *commands[], int *num_commands) {
     }
 }
 
-// Твой старый `main()` без изменений
+
 int main(int argc, char *argv[]) {
     if (argc < 2) {
         fprintf(stderr, "Использование: %s <команда>\n", argv[0]);
